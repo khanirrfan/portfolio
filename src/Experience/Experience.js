a87c1b37790e044ec3a1072a1952e221faf05493
irrfan13 <irfan.khan@enquero.com>
2019-07-29 18:12:30 +0530

    experience card added

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -3,8 +3,28 @@ import React, { Component } from 'react'
 export default class componentName extends Component {
     render() {
         return (
+            <div>
             <div className="headerClass">
             <h1>Experience </h1>
+            </div>
+            <div className="container">
+                <div className = "card">
+                    <div className="card-content">
+                           <div className="row">
+                                <div className="col-sm-12 col-md-2">Hello</div>
+                                <div className="col-sm-12 col-md-10">Hello</div>
+                           </div>
+                           <hr></hr>
+                           <div className="">
+                                        I will put experience section here
+                           </div>
+                    </div>
+                    <hr></hr>
+                    <div className="card-action">
+                        hello
+                    </div>
+                </div>
+            </div>
             </div>
         )
     }
